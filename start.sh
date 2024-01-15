5a236d201fc669d12f1c07a796e380ead2042337
Lazyomkar <156710936+Lazyomkar@users.noreply.github.com>
2024-01-16 03:42:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/Lazyomkar/Advance-Auto-Filter-Om /Advance-Auto-Filter-Om
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /Advance-Auto-Filter-Om
 fi
 cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
